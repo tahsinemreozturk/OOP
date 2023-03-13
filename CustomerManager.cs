f79460b992694db45246cdcbf7d3cf08d32d5a39
Tahsin Emre Öztürk <223010710040@kayseri.edu.tr>
2023-03-13 21:04:20 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    // is siniflari- bussines classes
+    internal class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using OOP2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Emre Ozturk
+        GercekMusteri musteri1 = new GercekMusteri();
+        musteri1.MusteriNo = "12345";
+        musteri1.Adi = "Emre";
+        musteri1.Soyad = "Ozturk";
+        musteri1.TcNo = "12345678";
+
+        //Ozturk Ticaret
+        TuzelMusteri musteri2 = new TuzelMusteri();
+        musteri2.Id = 1;
+        musteri2.MusteriNo = "21314";
+        musteri2.SirketAdi= "Ozturk Ticaret";
+        musteri2.VergiNo = "2345AQFV2";
+
+        Musteri musteri3= new GercekMusteri(); //new= bellekteki refernas no demek...
+        Musteri musteri4 = new TuzelMusteri();
+
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add(musteri1);
+        customerManager.Add(musteri2);
+
+
+
+
+
+
+
+
+        // kurumsallikta verim icin gercek musteri ve tuzel musteri ayrimi yapilmalidir.
+        // Gercek Nesne-Tuzel Nesne
+    }
+}
